6bdf4148ae565686d02e64e29a606b0f4661d121
SalemsPop <pekka.aho@hotmail.com>
2025-12-16 10:48:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ let persons = [
     }
 ]
 
+app.get('/', (req, res) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
@@ -106,4 +110,5 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
 
